53e5cc1d188bd8b9f72bea63cac1bcc01745a193
willcav <willcavenagli@gmail.com>
2024-07-16 21:16:18 -0300

    feat: move GEO_API_KEY to secret files

--- packages/app/ios/Runner/AppDelegate.swift
+++ packages/app/ios/Runner/AppDelegate.swift
@@ -1,13 +1,3 @@
-import UIKit
-import Flutter
-import GoogleMaps
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
   import UIKit
   import Flutter
   import GoogleMaps
@@ -18,22 +8,14 @@ import GoogleMaps
       _ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-      // Hardcoding API keys in the code is not a recommended practice for production apps.
-      // It's done here for the sake of simplicity in this challenge/demo app.
-      //
-      // In a production environment, i would recommended to:
-      // - Store the API key securely on a server and retrieve it at runtime.
-      // - Or, store it in a secure local configuration file that's not included in version control.
-      // - Use obfuscation tools to prevent reverse engineering of the app.
-      //
-      // Security is paramount in a production environment.
-      GMSServices.provideAPIKey("AIzaSyD2s6HoOsQ6sTVqtgc3ZP9TkeWM-zE86EU")
+      if let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+         let config = NSDictionary(contentsOfFile: path),
+         let apiKey = config["GEO_API_KEY"] as? String {
+            GMSServices.provideAPIKey(apiKey)
+      } else {
+          print("API Key not found!")
+      }
       GeneratedPluginRegistrant.register(with: self)
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
   }
-    GMSServices.provideAPIKey("AIzaSyD2s6HoOsQ6sTVqtgc3ZP9TkeWM-zE86EU")
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
